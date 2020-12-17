49e7dd9a7d6c20d78478df8a8d952000c8dd5dd1
A Team <68806181+ateamTSG@users.noreply.github.com>
2020-12-17 11:45:29 +0200

    Update pull-chart-and-deploy.sh

--- pull-chart-and-deploy.sh
+++ pull-chart-and-deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Dec 2020 Tzahi
 
 # IMPORTANT!
-## To install a Helm chart to Kubernetes, the chart must be in the local cache!!
+## To install a Helm chart to Kubernetes, the chart must be in the LOCAL cache!!
 
 ## Change Registry VAR!
 REG='XXX.azurecr.io'
 RELEASE_NAME='nginx-helloworld'
+# Making sure it doesn't deploy from local source from before script
 helm chart remove $REG/helm/nginx-deployment:latest
 
 ## Pulling from Registry to local:
